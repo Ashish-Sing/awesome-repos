44bd0dc258e12a764f5452d2959a3238350bcc09
DevOps_Eng <DevOps@example.com>
2023-07-24 12:07:09 +0000

    My first commit

---
+++ table.py
@@ -0,0 +1,11 @@
+for i in range(1, 11):
+
+    print(f"Multiplication table for {i}:")
+
+    for j in range(1, 11):
+
+        print(f"{i} x {j} = {i*j}")
+
+    print()  # Print an empty line after each table for better readability
+
+
